4680e93d896270ca99c8768d022ff01932a64bad
Funeng Wang <fortune_wang95@163.com>
2024-02-06 18:36:06 +0800

    Integration for Spring Security and Jwt

--- src/main/java/com/deliveroo/rider/controller/AuthController.java
+++ src/main/java/com/deliveroo/rider/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.deliveroo.rider.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AccountExpiredException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -23,15 +24,19 @@ public class AuthController {
     public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(loginRequest.getUserName(), loginRequest.getPassword());
-        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
-        if (authenticate == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login Failed!");
-        } else {
+        try{
+            Authentication authenticate = authenticationManager.authenticate(authenticationToken);
             Object principal = authenticate.getPrincipal();
             CustomUserDetails customUserDetails = (CustomUserDetails) principal;
             Account account = customUserDetails.getAccount();
             String token = JwtUtil.generateToken(account);
             return ResponseEntity.ok(token);
+        }catch (Exception e){
+            if(e instanceof AccountExpiredException){
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Account Expired!");
+            }else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
         }
     }
 }
